435f405234040d856dd16e33d4fdefda0c76d341
Zehra Tok <zehratok1@outlook.com>
2022-10-23 01:57:14 +0300

    Design the login screen for the user of type parent.

--- safekids/src/pages/parent/auth/Login/Login.js
+++ safekids/src/pages/parent/auth/Login/Login.js
@@ -1,11 +1,45 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, ImageBackground, Image } from 'react-native'
 import React from 'react'
-
+import styles from './Login.style';
+import { Stack, Pressable, Center, NativeBaseProvider, CheckIcon, CheckCircleIcon, Box, FormControl, StatusBar } from "native-base";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Input from '../../../../components/Input/TextInput'
+import Button from '../../../../components/Button/Button'
 const Login = () => {
+
+  const [show, setShow] = React.useState(false);
+
+
   return (
-    <View>
-      <Text>Parent Login</Text>
-    </View>
+    <ScrollView style={styles.container}
+      showsVerticalScrollIndicator={false}>
+      <StatusBar backgroundColor="#B0CFD5" barStyle="light-content" />
+      <ImageBackground
+        source={require('../../../../assets/images/background.png')}
+        style={styles.bg_image}>
+      </ImageBackground>
+
+      <View style={styles.bottom_view}>
+        <View style={styles.parents_image_view}>
+          <Image style={styles.parents_image} source={require('../../../../assets/images/parents.png')} />
+        </View>
+        <View style={styles.form_view}>
+          <NativeBaseProvider>
+            <Input label='E-posta Adresinizi Girin' placeholder="E-posta" leftIcon='email' />
+            <Input label='Parolanızı Girin' placeholder="Parola" leftIcon='lock'
+              type={show ? "text" : "password"}
+              onPress={() => setShow(!show)} rightIcon={show ? "eye" : "eye-off"} />
+            <Button text='Giriş Yap' />
+          </NativeBaseProvider>
+          <Text style={styles.question_text}> Hesabınız yok mu?
+            <Text style={styles.link_text}> {''} Hemen kaydolun. </Text>
+          </Text>
+        </View>
+      </View>
+
+    </ScrollView >
   )
 }
 
---
+++ safekids/src/pages/parent/auth/Login/Login.style.js
@@ -0,0 +1,50 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+    },
+    bg_image: {
+        height: Dimensions.get('window').height / 2.5,
+        backgroundColor: "#B0CFD5"
+    },
+    bottom_view: {
+        backgroundColor: '#ffffff',
+        bottom: Dimensions.get('window').height / 15,
+        borderTopStartRadius: 50,
+        borderTopEndRadius: 50,
+    },
+    parents_image_view: {
+        borderRadius: 100,
+        alignSelf: "center",
+        backgroundColor: '#B0CFD5',
+        position: 'absolute',
+        marginTop: -75,
+        width: 150,
+        height: 150,
+    },
+    parents_image: {
+        width: 120,
+        height: 150,
+    },
+    form_view: {
+        textAlign: "center",
+        padding: '10%',
+        marginTop: '20%'
+    },
+    question_text: {
+        textAlign: 'center',
+        fontSize: 13,
+        fontWeight: "bold",
+        color: '#D9AA60'
+    },
+    link_text: {
+        color: '#B88296',
+        fontSize: 12,
+        fontWeight: "bold",
+        fontStyle: 'italic',
+    },
+
+
+});
