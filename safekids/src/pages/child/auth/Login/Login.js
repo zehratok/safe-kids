b2bc09b3e539a0760d2f639d8ae875ddfb2a8cf8
Zehra Tok <zehratok1@outlook.com>
2022-10-23 18:53:40 +0300

    Design the login screen for the user of type child.

--- safekids/src/pages/child/auth/Login/Login.js
+++ safekids/src/pages/child/auth/Login/Login.js
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Image, ImageBackground, ScrollView, Text, View } from 'react-native'
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { Link } from '@react-navigation/native'
+import Button from '../../../../components/Button/Button'
+import Input from '../../../../components/Input/TextInput'
+import styles from './Login.style';
 
 const Login = () => {
+
+  const [show, setShow] = React.useState(false);
+
+
   return (
-    <View>
-      <Text> child Login</Text>
-    </View>
+    <ScrollView style={styles.container}
+      showsVerticalScrollIndicator={false}>
+      <StatusBar backgroundColor="#B0CFD5" barStyle="light-content" />
+      <ImageBackground
+        source={require('../../../../assets/images/background.png')}
+        style={styles.bg_image}>
+      </ImageBackground>
+      <View style={styles.bottom_view}>
+        <View style={styles.child_image_view}>
+          <Image style={styles.boy_image} source={require('../../../../assets/images/child_boy.png')} />
+          <Image style={styles.girl_image} source={require('../../../../assets/images/child_girl.png')} />
+        </View>
+        <View style={styles.form_view}>
+          <NativeBaseProvider>
+            <Input label='E-posta Adresinizi Girin' placeholder="E-posta" leftIcon='email' />
+            <Input label='Parolanızı Girin' placeholder="Parola" leftIcon='lock'
+              type={show ? "text" : "password"}
+              onPress={() => setShow(!show)} rightIcon={show ? "eye" : "eye-off"} />
+            <Link to={{ screen: 'Forgot Password' }} style={styles.forgot_password_view}>
+              <Text style={styles.forgot_password_text}>Parolanızı mı unuttunuz?</Text>
+            </Link>
+            <Button text='Giriş Yap' onPress={() => console.log("deneme")} />
+          </NativeBaseProvider>
+          <Text style={styles.question_text}> Hesabınız yok mu? {' '}
+            <Link to={{ screen: 'Child Register' }} >
+              <Text style={styles.register_link_text}>Hemen kaydolun.</Text>
+            </Link>
+          </Text>
+        </View>
+      </View>
+
+    </ScrollView >
   )
 }
 
---
+++ safekids/src/pages/child/auth/Login/Login.style.js
@@ -0,0 +1,70 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+    },
+    bg_image: {
+        height: Dimensions.get('window').height / 2.5,
+        backgroundColor: "#B0CFD5"
+    },
+    bottom_view: {
+        backgroundColor: '#ffffff',
+        bottom: Dimensions.get('window').height / 15,
+        borderTopStartRadius: 50,
+        borderTopEndRadius: 50,
+    },
+    child_image_view: {
+        borderRadius: 100,
+        alignSelf: "center",
+        backgroundColor: '#B0CFD5',
+        position: 'absolute',
+        marginTop: -75,
+        width: 150,
+        height: 150,
+    },
+    boy_image: {
+        marginLeft: -15,
+        width: 110,
+        height: 120,
+    },
+    girl_image: {
+        marginLeft: 5,
+        marginTop: -95,
+        width: 145,
+        height: 120,
+    },
+    form_view: {
+        textAlign: "center",
+        padding: '10%',
+        marginTop: '20%'
+    },
+    forgot_password_view: {
+        alignSelf: "flex-end",
+        marginRight: 5,
+        marginVertical: -5
+    },
+    forgot_password_text: {
+        color: '#B88296',
+        fontSize: 12,
+        fontWeight: "bold",
+        fontStyle: 'italic',
+    },
+    question_text: {
+        marginTop: 5,
+        textAlign: 'center',
+        fontSize: 13,
+        fontWeight: "bold",
+        color: '#D9AA60',
+    },
+    register_link_text: {
+        color: '#B88296',
+        fontSize: 12,
+        fontWeight: "bold",
+        fontStyle: 'italic',
+        textDecorationLine: 'underline',
+    },
+
+
+});
