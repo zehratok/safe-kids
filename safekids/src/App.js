23bbd6a8d56e538d7c2e53ab4352d75e1ff2d7ad
Zehra Tok <zehratok1@outlook.com>
2022-10-23 18:35:34 +0300

    Create the 'password reset' screen in the 'both' folder.

--- safekids/src/App.js
+++ safekids/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Welcome from './pages/both/welcome/Welcome'
+import Password from './pages/both/password_reset/Password'
 import ParentLogin from './pages/parent/auth/Login/Login'
 import ParentRegister from './pages/parent/auth/Register/Register'
 import ChildLogin from './pages/child/auth/Login/Login'
@@ -14,10 +15,12 @@ export default () => {
     return (
       <Stack.Navigator screenOptions={{headerShown:false}}>
         <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Forgot Password" component={Password}/>
         <Stack.Screen name="Parent Login" component={ParentLogin} />
         <Stack.Screen name="Parent Register" component={ParentRegister} />
         <Stack.Screen name="Child Login" component={ChildLogin} />
         <Stack.Screen name="Child Register" component={ChildRegister} />
+
       </Stack.Navigator>
     )
   }
---
+++ safekids/src/pages/both/password_reset/Password.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Password = () => {
+  return (
+    <View>
+      <Text>Password</Text>
+    </View>
+  )
+}
+
+export default Password
