efcb3cb0918cc57594703986b2810913c6f08d0d
Zehra Tok <zehratok1@outlook.com>
2022-11-08 10:47:58 +0300

    Create files of Chat and Settings screens.

---
+++ safekids/src/pages/child/app/Chat/Chat.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './Chat.style'
+
+const Chat = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Chat</Text>
+        </View>
+    )
+}
+export default Chat
---
+++ safekids/src/pages/parent/app/Chat/Chat.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import colors from "styles/colors";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.main_white,
+        justifyContent: "center",
+    },
+});
+export default styles;
